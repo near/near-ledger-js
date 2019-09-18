6337add1c27a531a51d2ea944758dfd16615f0a5
Vladimir Grichina <vgrichina@gmail.com>
2019-09-17 23:28:06 -0700

    Implement transaction signing

--- demo.js
+++ demo.js
@@ -4,6 +4,8 @@ import "regenerator-runtime/runtime";
 import TransportU2F from "@ledgerhq/hw-transport-u2f";
 import { listen } from "@ledgerhq/logs";
 
+import bs58 from "bs58"
+
 listen(console.log);
 
 export async function createTransport() {
@@ -27,6 +29,8 @@ function bip32PathToBytes(path) {
             ])));
 }
 
+const networkId = 'W'.charCodeAt(0);
+
 export async function createClient() {
     const transport = await createTransport();
     return {
@@ -37,9 +41,22 @@ export async function createClient() {
             return `${major}.${minor}.${patch}`;
         },
         async getPublicKey(path) {
-            const networkId = 'W'.charCodeAt(0);
             const response = await this.transport.send(0x80, 4, 0, networkId, bip32PathToBytes(path));
             return response;
+        },
+        async sign(path, transactionData) {
+            transactionData = Buffer.from(bs58.decode(transactionData))
+            // 128 - 5 service bytes
+            const CHUNK_SIZE = 123
+            const allData = Buffer.concat([bip32PathToBytes(path), transactionData]);
+            for (let offset = 0; offset < allData.length; offset += CHUNK_SIZE) {
+                const chunk = Buffer.from(allData.subarray(offset, offset + CHUNK_SIZE));
+                const isLastChunk = offset + CHUNK_SIZE >= allData.length;
+                const response = await this.transport.send(0x80, 2, isLastChunk ? 0x80 : 0, networkId, chunk);
+                if (isLastChunk) {
+                    return Buffer.from(response.subarray(0, -2));
+                }
+            }
         }
     }
 }
