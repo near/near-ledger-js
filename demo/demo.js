97b2511897ce6a619edd3ebed67ecc9ed805068a
Vladimir Grichina <vgrichina@gmail.com>
2020-05-18 22:13:29 -0700

    Fix demo app

--- demo/demo.js
+++ demo/demo.js
@@ -1,10 +1,23 @@
+import "regenerator-runtime/runtime";
+
+import TransportU2F from "@ledgerhq/hw-transport-u2f";
 import { listen } from "@ledgerhq/logs";
 import bs58 from "bs58";
 
-import { createTransport, createClient } from "../";
+import { createClient } from "../";
 
 listen(console.log);
 
-Object.assign(window, { createTransport, createClient, bs58, Buffer });
+async function createTransport() {
+    const transport = await TransportU2F.create();
+    transport.setScrambleKey("NEAR");
+    return transport;
+}
 
-createClient().then(client => window.client = client);
+Object.assign(window, { createClient, createTransport, bs58, Buffer });
+
+(async () => {
+    const transport = await createTransport();
+    const client = await createClient(transport);
+    Object.assign(window, { transport, client });
+})().catch(console.error);
