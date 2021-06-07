a3350cf9330943f37422446661b40e8eb1ae0591
Daryl Collins <daryl@darylcollins.com>
2021-06-07 14:02:01 -0400

    1775 - [WiP] - Ledger transport fallback support

--- demo/demo.js
+++ demo/demo.js
@@ -1,23 +1,26 @@
 import "regenerator-runtime/runtime";
 
-import TransportU2F from "@ledgerhq/hw-transport-u2f";
 import { listen } from "@ledgerhq/logs";
 import bs58 from "bs58";
 
-import { createClient } from "../";
+import { createClient, getSupportedTransport } from "../";
 
 listen(console.log);
 
 async function createTransport() {
-    const transport = await TransportU2F.create();
+    const transport = await getSupportedTransport();
     transport.setScrambleKey("NEAR");
     return transport;
 }
 
-Object.assign(window, { createClient, createTransport, bs58, Buffer });
-
-(async () => {
-    const transport = await createTransport();
-    const client = await createClient(transport);
-    Object.assign(window, { transport, client });
-})().catch(console.error);
+Object.assign(window, {
+    initialize: async function initialize() {
+        const transport = await createTransport();
+        const client = await createClient(transport);
+        Object.assign(window, { transport, client });
+    },
+    createClient,
+    createTransport,
+    bs58,
+    Buffer
+});
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const { default: LedgerTransportU2F } = require('@ledgerhq/hw-transport-u2f');
+const { default: LedgerTransportWebUsb } = require('@ledgerhq/hw-transport-webusb');
+const { default: LedgerTransportWebHid } = require('@ledgerhq/hw-transport-webhid');
+const platform = require('platform');
+
+const ENABLE_DEBUG_LOGGING = false;
+const debugLog = (...args) => {
+    ENABLE_DEBUG_LOGGING && console.log(...args)
+};
 
 function bip32PathToBytes(path) {
     const parts = path.split('/');
@@ -17,7 +26,55 @@ function bip32PathToBytes(path) {
 const networkId = 'W'.charCodeAt(0);
 
 const DEFAULT_PATH = "44'/397'/0'/0'/1'";
- 
+
+// Basic structure transferred from https://github.com/vacuumlabs/adalite
+async function isWebUsbSupported() {
+    const isSupported = await LedgerTransportWebUsb.isSupported();
+    return isSupported && platform.os.family !== 'Windows' && platform.name !== 'Opera';
+}
+
+async function isWebHidSupported() {
+    return LedgerTransportWebHid.isSupported();
+}
+
+module.exports.getSupportedTransport = async function getSupportedTransports() {
+    let transport;
+
+    try {
+        debugLog('checking webHID support');
+        const supportWebHid = await isWebHidSupported();
+        debugLog('checking webUSB support');
+        const supportWebUsb = await isWebUsbSupported();
+
+        debugLog("Will try:", { supportWebUsb, supportWebHid });
+        if (supportWebHid) {
+            // WebHID is still supported by latest Chrome, so try that first
+            transport = await LedgerTransportWebHid.create();
+        } else if (supportWebUsb) {
+            transport = await LedgerTransportWebUsb.create();
+        } else {
+            // Firefox/Mozilla intend to not support WebHID or WebUSB
+            transport = await LedgerTransportU2F.create();
+        }
+    } catch (hwTransportError) {
+        try {
+            console.warn('failed to initialize transport!', { hwTransportError })
+            // fallback to U2F in case of errors detecting or creating preferred transports
+            transport = await LedgerTransportU2F.create();
+        } catch (u2fTransportError) {
+            console.error('Failed to initialize transport', {
+                hwTransportError: { name: hwTransportError.name, message: hwTransportError.message },
+                u2fTransportError: { name: u2fTransportError.name, message: u2fTransportError.message }
+            });
+
+            throw hwTransportError;
+        }
+    }
+
+    debugLog('Transport created!', transport);
+    return transport;
+}
+
 module.exports.createClient = async function createClient(transport) {
     return {
         transport,
